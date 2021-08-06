77e4d7f4d10f1d16d8b1964f36ee3094ea0e1d0a
Jessica Buckwalter <jessica.a.buckwalter@gmail.com>
2021-08-06 15:40:24 -0500

    Tested link between html and js files

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,7 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+
+// Before anything I look in the index.html file to see if the .js file is linked.
+// I also open the index.html file in the browser and check the console to see
+// the above log to see what they are successfully linked.

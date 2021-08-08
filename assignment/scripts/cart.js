e1672d9d95e0155d80c2af26eecf50734a34740e
Jessica Buckwalter <jessica.a.buckwalter@gmail.com>
2021-08-08 10:23:59 -0500

    Created basket function and tested output. Successful output

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -5,3 +5,21 @@ console.log('***** Cart Functions *****');
 // Before anything I look in the index.html file to see if the .js file is linked.
 // I also open the index.html file in the browser and check the console to see
 // the above log to see what they are successfully linked.
+ let basket = [];
+
+ function addItem ( item ) {
+   basket.push( item );
+   if ( item ) {
+     return true;
+   }
+   return false;
+ }
+
+// console.log( addItem( 'bananas' ), basket );
+// tested adding an item, then logged the array to show the item.
+
+function listItems () {
+  for (let i=0; i<basket.length; i+=1 ){
+
+  }
+}
